74e971b3af33a1f96e949533feab8166d8412f18
Ahmed Mahmoud <153916812+A7emdMa7moud@users.noreply.github.com>
2024-12-15 12:56:07 +0200

    update

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://e-commerce-frontend-4.vercel.app/",
+    origin: "https://e-commerce-frontend-4.vercel.app",
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
     methods: ["GET", "POST", "PUT", "DELETE"],
