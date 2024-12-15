6f4bad44eb3075b0925803d7ca58ee30589a34cb
Ahmed Mahmoud <153916812+A7emdMa7moud@users.noreply.github.com>
2024-12-15 12:47:01 +0200

    update new frontend url

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://backend-flax-zeta.vercel.app"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
     methods: ["GET", "POST", "PUT", "DELETE"],
