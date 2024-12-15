b1d63b5780187c68fa5b577807129baff0bee494
Ahmed Mahmoud <153916812+A7emdMa7moud@users.noreply.github.com>
2024-12-15 13:37:53 +0200

    add gloubal url

--- index.js
+++ index.js
@@ -17,7 +17,10 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "https://e-commerce-frontend-4.vercel.app",
+    ],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
     methods: ["GET", "POST", "PUT", "DELETE"],
